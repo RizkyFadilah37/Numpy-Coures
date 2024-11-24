6d59257925d01f8a392a9a2131d29d671fea7faa
Kakania <sallysite123@gmail.com>
2024-11-24 20:51:18 +0700

    docs :Instalation library Numpy on python

---
+++ Instalation Numpy/main.py
@@ -0,0 +1,10 @@
+# Import library numpy
+import numpy as np
+
+# Create a numpy array
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+
+# Different between numpy array and list
+b = [1, 2, 3, 4, 5]
+print(b)
